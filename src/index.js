3330661e48662181ebfe30dca89a54fded30831e
StrFl <ghgod123321@gmail.com>
2023-10-25 13:09:24 +0300

    asdasdads

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components//App/App';
 import reportWebVitals from './reportWebVitals';
+import Main from './components/Main/Main';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
