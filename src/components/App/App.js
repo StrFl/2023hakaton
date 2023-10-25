4eb93a0b6875009592466d04e4604a8f21f62892
StrFl <ghgod123321@gmail.com>
2023-10-25 13:01:51 +0300

    2c

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: 'Test'
+    };
+  }
+  
+  render() {
+
+
+    return (
+    <div className="head">
+      <p className="h-title">Проект пользователя { this.state.user }</p>
+      </div>
+      
+
+
+  );}
+
+  
+}
+
+export default App;
