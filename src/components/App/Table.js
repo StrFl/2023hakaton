ef98c028928cfec8171afe1f64e314bb63f10bf6
StrFl <ghgod123321@gmail.com>
2023-10-26 18:27:04 +0300

    asd

--- src/components/App/Table.js
+++ src/components/App/Table.js
@@ -68,67 +68,44 @@ const Table = ({ todos, isLoading, setTodos }) => {
   return (
     <div>
       
-      <table className="w-11/12 max-w-4xl">
-        <thead className="border-b-2 border-black">
-          <tr>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              Checkbox
-            </th>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              To Do
-            </th>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              Описание
-            </th>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              Status
-            </th>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              Date Created
-            </th>
-            <th className="p-3 text-sm font-semibold tracking-wide text-left">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody>
+    
           {isLoading ? (
-            <div>Is Loading </div>
+            <div>Загрузка </div>
           ) : (
             <>
               {" "}
               {todos.map((todoItem, index) => (
-                <tr key={todoItem.id} className="border-b border-black">
-                  <td className="p-3">
-                    <span
+                <tr key={todoItem.id} className="line">
+                
+                    <p
                       onClick={() =>
                         handleCheckbox(todoItem.id, todoItem.completed)
                       }
-                      className="inline-block cursor-pointer"
+                      className="line"
                     >
                       {todoItem.completed === true ? (
                         <MdOutlineCheckBox />
                       ) : (
                         <MdOutlineCheckBoxOutlineBlank />
                       )}
-                    </span>
-                  </td>
-                  <td className="p-3 text-sm " title={todoItem.id}>
+                    </p>
+               
+                  <p className="p-3 text-sm " title={todoItem.id}>
                     {todoItem.prioritet}
-                  </td>
-                  <td className="p-3 text-sm " title={todoItem.id}>
+                  </p>
+                  <p className="p-3 text-sm " title={todoItem.id}>
                     {todoItem.body}
-                  </td>
-                  <td className="p-3 text-sm " title={todoItem.id}>
+                  </p>
+                  <p className="p-3 text-sm " title={todoItem.id}>
                     {todoItem.goals}
-                  </td>
+                  </p>
                   <td className="p-3 text-sm text-center">
                     <span
                       className={`p-1.5 text-xs font-medium tracking-wider rounded-md ${
                         todoItem.completed ? "bg-green-300" : "bg-red-300"
                       }`}
                     >
-                      {todoItem.completed ? "Done" : "Incomplete"}
+                      {todoItem.completed ? "Выполнено" : "В процессе"}
                     </span>
                   </td>
                   <td className="p-3 text-sm font-medium">
@@ -153,35 +130,12 @@ const Table = ({ todos, isLoading, setTodos }) => {
               ))}
             </>
           )}
-        </tbody>
-      </table>
+    
 
-      {/* Modal */}
-      <input type="checkbox" id="my-modal" className="modal-toggle" />
-      <div className="modal">
-        <div className="modal-box">
-          <h3 className="font-bold text-lg">Edit Todo</h3>
-          <input
-            type="text"
-            value={editText.body}
-            onChange={handleChange}
-            placeholder="Type here"
-            className="input input-bordered w-full mt-8"
-          />
-          <div className="modal-action">
-            <label
-              htmlFor="my-modal"
-              onClick={handleClick}
-              className="btn btn-primary"
-            >
-              Edit
-            </label>
-            <label htmlFor="my-modal" className="btn">
-              Close
-            </label>
-          </div>
-        </div>
-      </div>
+    
+      
+  
+   
     </div>
   );
 };
